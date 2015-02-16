ebe0edc632a686a577f02aa89e54b39cb4caf5f9
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2015-02-15 22:52:55 -0800

    On video 40

--- controller/create-db.php
+++ controller/create-db.php
@@ -3,16 +3,32 @@
 require_once(__DIR__ . "/../model/config.php");
 
 $query = $_SESSION["connection"]->query("CREATE TABLE posts ("
-        . "id int(11) NOT NULL AUTO_INCREMENT,"
-        . "title varchar(255) NOT NULL,"
-        . "post text NOT NULL,"
-        . "PRIMARY KEY (id))");
+. "id int(11) NOT NULL AUTO_INCREMENT,"
+. "title varchar(255) NOT NULL,"
+. "post text NOT NULL,"
+. "PRIMARY KEY (id))");
 
 echo $query;
 //Here we asked if the table was created or not and here it says it is created successfully.
 if ($query) {
-    echo "<p>Successfully created table: posts</p>";
+echo "<p>Successfully created table: posts</p>";
 }
+//This is telling us if we have an error it will appear and currently we do not have an error.
 else {
-    echo "<p>" .$_SESSION["connection"]->error . "</p>";
+echo "<p>" .$_SESSION["connection"]->error . "</p>";
+}
+
+$query = $_SESSION["connection"]->query("CREATE TABLE users("
+. "id int(11) NOT NULL AUTO_INCREMENT, "
+. "username varchar(30) NOT NULL,"
+. "email varchar(50) NOT NULL,"
+. "salt char(128) NOT NULL,"
+. "PRIMATY KEY(id))");
+
+if($query) {
+echo "<p>Successfully created table: users</p>";
+}
+
+else {
+     echo "<p>" . $_SESSION["connection"]->error . "</p>";
 }
