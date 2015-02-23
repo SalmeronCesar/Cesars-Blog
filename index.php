9a120341f9090f1307589fd8a382113ce5bd7ee1
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2015-02-23 00:00:04 -0800

    Worked on the blog a little

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
-<link type="text/css" rel="stylesheet" href="css/custom-style.css">
+<link type="text/css" rel="stylesheet" href="css/custom-style.css"></link>
+<img class="img-responsive img-rounded" src="http://www.flightcentre.com.au/global-images/Product_Images/New/Cruise_Website/images/Destinations/Be_Inspired/Tropical_islands/Destinations_Be_Inspired_Tropical_islands_Island3.jpg">
 <!-- start navmenu --> 
-<body>
-<ul class="nav nav-pills">
-    <li class="active"><a href="index.php">Home</a></li>
-    <li><a title="Register/Login" href="login.php">Register/Login</a></li>
-</ul>  
-<!-- end navmenu -->
-</body>
+<style>
+                   body{ background:none; margin: 0px; }
+            div#nav{padding: 24px;}
+            div#nav > a{
+                font-family: Ariel, Helvetica, sans-serif;
+                font-size: 25px;
+                background: whitesmoke;
+                padding: 12px 24px;
+                color: black;
+                text-decoration: none;
+                margin-left: 10px;
+                border-radius: 3px;
+            }
+            div#nav > a:hover{
+                background: black;
+                color: whitesmoke;
+            }
+        </style>
+            
+            <div id="nav">
+                <a class="active"  href="index.php">Home</a><a href="login.php">Register/Login</a>
+                <!-- end navmenu -->
 <?php
 require_once(__DIR__ . "/controller/login-verify.php");
 require_once(__DIR__ . "/view/header.php");
@@ -18,4 +34,4 @@ require_once(__DIR__ . "/view/footer.php");
 require_once(__DIR__ . "/controller/read-posts.php");
 ?>
 
-<div align="center" style="z-index:9;visibility:visible;"><a href="http://www.totallyfreecursors.com/" target="_blank" class="tfc"></a></div><style>HTML,BODY{cursor: url("http://downloads.totallyfreecursors.com/cursor_files/fireorange.ani"), url("http://downloads.totallyfreecursors.com/thumbnails/fireorange.gif"), auto;}</style>*/
+<div align="center" style="z-index:9;visibility:visible;"><a href="http://www.totallyfreecursors.com/" target="_blank" class="tfc"></a></div><style>HTML,BODY{cursor: url("http://downloads.totallyfreecursors.com/cursor_files/fireorange.ani"), url("http://downloads.totallyfreecursors.com/thumbnails/fireorange.gif"), auto;}</style>
--- login.php
+++ login.php
@@ -1,12 +1,28 @@
-<link type="text/css" rel="stylesheet" href="css/custom-style.css">
+<link type="text/css" rel="stylesheet" href="css/custom-style.css"></link>
 <!-- start navmenu --> 
-<body>
-<ul class="nav nav-pills">
-    <li class="active"><a href="login.php">Register/Login</a></li>
-    <li><a title="" href="index.php">Home</a></li>
-</ul>
-<!-- end navmenu -->
-</body>
+<style>
+                   body{ background:none; margin: 0px; }
+            div#nav{padding: 24px;}
+            div#nav > a{
+                font-family: Ariel, Helvetica, sans-serif;
+                font-size: 25px;
+                background: whitesmoke;
+                padding: 12px 24px;
+                color: black;
+                text-decoration: none;
+                margin-left: 10px;
+                border-radius: 3px;
+            }
+            div#nav > a:hover{
+                background: black;
+                color: whitesmoke;
+            }
+        </style>
+            
+            <div id="nav">
+                <a class="active"  href="login.php">Register/Login</a><a href="index.php">Home</a>
+                <!-- end navmenu -->
+               <div align="center" style="z-index:9;visibility:visible;"><a href="http://www.totallyfreecursors.com/" target="_blank" class="tfc"></a></div><style>HTML,BODY{cursor: url("http://downloads.totallyfreecursors.com/cursor_files/fireorange.ani"), url("http://downloads.totallyfreecursors.com/thumbnails/fireorange.gif"), auto;}</style>
     <?php
 
 require_once(__DIR__ . "/view/header.php");
